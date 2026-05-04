3cb99db7fbc84a4d847cd0dec569a96c6b425e78
ebrahime324 <82955695+ebrahime324@users.noreply.github.com>
2026-05-04 18:05:51 +0300

    fix seeders and admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,18 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@site.com',
+            'password' => Hash::make('123456'),
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,27 +1,14 @@
 <?php
-
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::query()->create([
-            'name' => 'system admin',
-            'email' => 'ebrahime131alaa@gmail.com',
-       'password' => Hash::make('password'),
+        $this->call([
+            AdminSeeder::class,
         ]);
     }
 }
